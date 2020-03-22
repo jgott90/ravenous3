eee4ef75186b37c3f0c6297e6ac2f280e65fa074
jgott90 <41353230+jgott90@users.noreply.github.com>
2020-03-22 15:20:02 -0500

    change logo

--- src/components/LandingPage/SearchSuggestions/SearchSuggestions.jsx
+++ src/components/LandingPage/SearchSuggestions/SearchSuggestions.jsx
@@ -3,9 +3,6 @@ import styles from './SearchSuggestions.module.css';
 
 export function SearchSuggestions() {
     return (
-       /* <div className={styles.suggestions}>
-            <span className="icon is-small"><i className="fas fa-search"></i><span>Restaurants</span></span>
-        </div>*/
         <div className={styles.suggestions}>
             <span className="icon is-small"><i className="fas fa-utensils"></i></span><span className={styles.suggestion}>Restaurants</span>
             <span className="icon is-small"><i className="fas fa-cocktail"></i></span><span className={styles.suggestion}>Nightlife</span>
