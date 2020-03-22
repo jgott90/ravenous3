049eb5d6278db02041918c0bf17d40e015ef8d44
jgott90 <41353230+jgott90@users.noreply.github.com>
2020-03-22 14:59:58 -0500

    adding Search Suggestions and icons

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { TopNav } from './TopNav/TopNav';
+import logo from '../../assets/logo.png';
+import styles from './LandingPage.module.css';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchSuggestions } from './SearchSuggestions/SearchSuggestions';
+
+
+export function LandingPage() {
+    return ( 
+    <div >
+        <TopNav/> { /* display as 1st child element of landing page */ }
+         <img src = { logo } className = { styles.logo } alt = 'logo' />
+        <SearchBar/>
+        <SearchSuggestions/>
+        </div>
+    );
+}
---
+++ src/components/LandingPage/SearchSuggestions/SearchSuggestions.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './SearchSuggestions.module.css';
+
+export function SearchSuggestions() {
+    return (
+       /* <div className={styles.suggestions}>
+            <span className="icon is-small"><i className="fas fa-search"></i><span>Restaurants</span></span>
+        </div>*/
+        <div className={styles.suggestions}>
+            <span className="icon is-small"><i className="fas fa-utensils"></i></span><span className={styles.suggestion}>Restaurants</span>
+            <span className="icon is-small"><i className="fas fa-cocktail"></i></span><span className={styles.suggestion}>Nightlife</span>
+            <span className="icon is-small"><i className="fas fa-concierge-bell"></i></span><span className={styles.suggestion}>Services</span>
+            <span className="icon is-small"><i className="fas fa-truck"></i></span><span className={styles.suggestion}>Delivery Services</span>
+        </div>
+    );
+}
