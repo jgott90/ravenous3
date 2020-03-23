e72c236af59fa77ded16b8b3b7c7f05c42226957
jgott90 <41353230+jgott90@users.noreply.github.com>
2020-03-23 18:45:58 -0500

    update subnav divs

---
+++ src/components/NavBar/SubNav/SubNav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SubNavItem } from './SubNavItem/SubNavItem';
+import styles from './SubNav.module.css';
+
+export function SubNav() {
+    return (
+        <div>
+            <SubNavItem label='Restaurants' icon='fa-utensils'/>
+            <SubNavItem label ='Home Services' icon='fa-home'/>
+            <SubNavItem label = 'AutoServices' icon='fa-car'/>
+            <SubNavItem label = 'More' icon='fa-info'/>
+        </div>
+    );
+}
