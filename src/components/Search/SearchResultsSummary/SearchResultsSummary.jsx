3732db6f07a5f80863bdb152af5a42c3bac49b9e
jgott90 <41353230+jgott90@users.noreply.github.com>
2020-03-24 00:37:31 -0500

    add search results buttons

--- src/components/Search/SearchResultsSummary/SearchResultsSummary.jsx
+++ src/components/Search/SearchResultsSummary/SearchResultsSummary.jsx
@@ -8,6 +8,35 @@ export function SearchResultsSummary() {
                 <h1 class="subtitle"><strong>burgers</strong> berlin</h1>
                 <p>Showing 1-20 out of 543 results</p>
             </div>
+            <div className={styles.filters}>
+                <button className={`button ${styles['subnav-button']}`}>
+                    <span className="icon">
+                        <i className="fas fa-sliders-h"></i>
+                    </span>
+                    <span>All Filters</span>
+                </button>
+
+                <div class="buttons has-addons">
+                    <button className="button">$</button>
+                    <button className="button">$$</button>
+                    <button className="button">$$$</button>
+                    <button className="button">$$$$</button>
+                </div>
+
+                <button className={`button ${styles['subnav-button']}`}>
+                    <span className="icon">
+                        <i className="fas fa-clock"></i>
+                    </span>
+                    <span>Open Now</span>
+                </button>
+
+                <button className={`button ${styles['subnav-button']}`}>
+                    <span className="icon">
+                        <i className="fas fa-dollar-sign"></i>
+                    </span>
+                    <span>Cashback</span>
+                </button>
+            </div>
         </div>
 
     );
