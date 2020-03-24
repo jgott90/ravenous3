5b4ab4e01bc2639ce64d672a71f758cbc8af00ca
jgott90 <41353230+jgott90@users.noreply.github.com>
2020-03-23 23:52:46 -0500

    add review buttons to subnav

--- src/components/NavBar/SubNav/SubNav.jsx
+++ src/components/NavBar/SubNav/SubNav.jsx
@@ -4,11 +4,30 @@ import styles from './SubNav.module.css';
 
 export function SubNav() {
     return (
-        <div>
-            <SubNavItem label='Restaurants' icon='fa-utensils'/>
-            <SubNavItem label ='Home Services' icon='fa-home'/>
-            <SubNavItem label = 'AutoServices' icon='fa-car'/>
-            <SubNavItem label = 'More' icon='fa-info' showRightBorder/>
+        <div className={styles.container}>
+            <div className={styles['sub-nav']}>
+                <div>
+                    <SubNavItem label='Restaurants' icon='fa-utensils' />
+                    <SubNavItem label='Home Services' icon='fa-home' />
+                    <SubNavItem label='AutoServices' icon='fa-car' />
+                    <SubNavItem label='More' icon='fa-info' showRightBorder />
+                </div>
+                <div>
+                    <button className={`button ${styles['subnav-button']} ${styles['omit-right-border']}`}>
+                        <span className="icon">
+                            <i className="fas fa-pen"></i>
+                        </span>
+                        <span>Write a Review</span>
+                    </button>
+
+                    <button className={`button ${styles['subnav-button']}`}>
+                        <span className="icon">
+                            <i className="fas fa-hotel"></i>
+                        </span>
+                        <span>For Businesses</span>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
